1880b2cbe7b72d31ca8da4f6a1304ace4f96fbbc
EldadCodere <eldad.yikne@codere.com>
2025-05-05 10:07:02 +0300

    asd

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
-const axios = require("axios");
+const cors = require("cors");
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
-const CHANNEL_ID = process.env.CHANNEL_ID;
-const ALIEXPRESS_APP_KEY = process.env.ALIEXPRESS_APP_KEY;
-const ALIEXPRESS_TRACKING_ID = process.env.ALIEXPRESS_TRACKING_ID;
+// const BOT_TOKEN = process.env.BOT_TOKEN;
+// const CHANNEL_ID = process.env.CHANNEL_ID;
+// const ALIEXPRESS_APP_KEY = process.env.ALIEXPRESS_APP_KEY;
+// const ALIEXPRESS_TRACKING_ID = process.env.ALIEXPRESS_TRACKING_ID;
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -12,7 +12,11 @@ const telegramRoutes = require("./routes/telegramRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(
+  cors({
+    origin: "http://localhost:5173", // or use '*' to allow all
+  })
+);
 app.use(bodyParser.json());
 
 // Register routes
@@ -24,3 +28,7 @@ app.listen(PORT, () => {
 
 // התחלת הפרסום
 // startPosting();
+
+// Allow all origins (or specify your own for tighter security)
+
+// Your existing routes
